8ca493b169de995528c8e084ea4bd5fd1ec5a4a7
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-05-08 20:47:35 -0300

    upload dos arquivos

---
+++ src/main/java/dev/mrodrigochaves/roteiro/RoteiroApplication.java
@@ -0,0 +1,13 @@
+package dev.mrodrigochaves.roteiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoteiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoteiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mrodrigochaves/roteiro/RoteiroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mrodrigochaves.roteiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoteiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
